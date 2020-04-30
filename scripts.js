3f5cc566b871e0a4bceaada7d1b828e427267d09
Tereza Zelená <terezka.zelena@seznam.cz>
2020-04-30 18:09:51 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
+// Third part:
 console.log('Script file is loaded');
+
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+
+let name = 'Adam';
+let age = 34;
+
+function myLittleFunction() {
+  $('main').append('<p>A p is appended in the main.</p>');
+  $('p').css('border', '1px solid red');
+  $('p').css('padding', '10px');
+}
+
+myLittleFunction();
+myLittleFunction();
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+  $('p:last-child').remove();
+});
 
 // Single line comment
 
